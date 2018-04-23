933a904c2db8803b4e4ee85498fba6a13608082d
sebbedelius <sebbe_fj@hotmail.com>
2018-04-23 14:44:03 +0200

    Testa detta

--- BoxModelHandler.cpp
+++
@@ -1,9 +0,0 @@
-#include "BoxModelHandler.h"
-
-BoxModelHandler::BoxModelHandler(Geometry* in_Geometry)
-{
-	this->geometry = in_Geometry;
-	this->boxModels = new BoxModel[];
-
-	this->initiateBoxModels();
-}
